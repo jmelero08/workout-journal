3d20d46bc3b86f277d280babcaba6975932a1bb5
Jose Melero Martinez <jmelero08@gmail.com>
2021-02-13 12:37:28 -0700

    added edit page and redirects

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,13 +1,12 @@
 class WorkoutController < ApplicationController
 
     get '/workout_entries/new' do
+        redirect_if_not_logged_in
         erb :'/workout_entries/new'
     end 
 
     post '/workout_entries' do
-        if !logged_in?
-            redirect '/'
-        end
+        redirect_if_not_logged_in
 
         if params[:exercise] != ""
             @workout_entry = WorkoutEntry.create(exercise: params[:exercise], user_id: current_user.id, reps: params[:reps]) 
@@ -18,11 +17,38 @@ class WorkoutController < ApplicationController
     end 
 
     get '/workout_entries/:id' do
-        @workout_entry = WorkoutEntry.find(params[:id])
+        set_workout_entry
         erb :'/workout_entries/show'
     end
 
     get '/workout_entries/:id/edit' do
-        erb :'/workout_entries/edit'
+        redirect_if_not_logged_in
+        set_workout_entry
+        if authorized_to_edit?(@workout_entry)
+            erb :'/workout_entries/edit'
+        else
+            redirect "users/#{current_user.id}"
+        end
       end
+
+    patch '/workout_entries/:id' do
+        redirect_if_not_logged_in
+        set_workout_entry
+        if @workout_entry.user == current_user && params[:exercise] != "" && params[:reps] != ""
+            @workout_entry.update(exercise: params[:exercise], reps: params[:reps])
+            redirect "/workout_entries/#{@workout_entry.id}"
+        else
+            redirect "users/#{current_user.id}"
+        end
+    end
+
+
+    private 
+
+    def set_workout_entry
+        @workout_entry = WorkoutEntry.find(params[:id])
+    end
 end
+
+
+
