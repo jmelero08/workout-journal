0048e1876ab8f6f5d4e787c5ff6601533b387c46
Jose Melero Martinez <jmelero08@gmail.com>
2021-02-21 17:35:45 -0700

    added flash messages and errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,14 @@ class UsersController < ApplicationController
 
     post '/login' do
         @user = User.find_by(email: params[:email])
-        if @user.authenticate(params[:password])
+        if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
             puts session
+            flash[:message] = "Welcome back, #{@user.name}!"
             redirect "users/#{@user.id}"
         else
-            
+            flash[:errors] = "No user name found, please sign up!"
+            redirect '/login'
         end 
     end
 
@@ -23,15 +25,17 @@ class UsersController < ApplicationController
         if params[:name] != "" && params[:email] != "" && params[:password] != ""
             @user = User.create(params)
             session[:user_id] = @user.id
+            flash[:message] = "You can now start trakcing your workouts!"
             redirect "/users/#{@user.id}"
         else
+            flash[:message] = "Credentials are invalide, please try again."
             redirect '/signup'
         end 
     end
 
     get '/users/:id' do
         @user = User.find_by(id: params[:id])
-        # redirect_if_not_logged_in
+        redirect_if_not_logged_in
         erb :'/users/show'
     end
 
--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -16,8 +16,10 @@ class WorkoutController < ApplicationController
 
         if params[:exercise] != ""
             @workout_entry = WorkoutEntry.create(exercise: params[:exercise], user_id: current_user.id, reps: params[:reps]) 
+            flash[:message] = "Workout successfully created!"
             redirect "/workout_entries/#{@workout_entry.id}"
         else
+            flash[:errors] = "Oops, something went wrong, make sure everything is filled out"
             redirect '/workout_entries/new'
         end 
     end 
@@ -52,9 +54,10 @@ class WorkoutController < ApplicationController
         set_workout_entry
         if authorized_to_edit?(@workout_entry)
             @workout_entry.destroy
-            #flash[:message] = "Successfully deleted this workout."
+            flash[:message] = "Successfully deleted this workout."
             redirect '/workout_entries'
         else 
+            flash[:errors] = "Cannot delete someone else's workout!"
             redirect '/workout_entries'
         end 
     end
@@ -66,6 +69,5 @@ class WorkoutController < ApplicationController
         @workout_entry = WorkoutEntry.find(params[:id])
     end
 end
-
 
 
